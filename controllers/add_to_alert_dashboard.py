7af5f1e78763a0bbb80d8cd67bd9e44dffe0d849
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2019-01-10 16:48:56 +0100

    Update "Add to my alert board" for Odoo 11
    --HG--
    branch : 11.0

--- controllers/add_to_alert_dashboard.py
+++ controllers/add_to_alert_dashboard.py
@@ -8,6 +8,8 @@ class AddToAlertDashboardController(Controller):
     dashboard added by this module.
 
     Similar to the one for the default dashboard in the "board" module.
+
+    Ref: odoo/addons/board/controllers/main.py
     """
 
     @route(route='/board/add_to_alert_dashboard', type='json', auth='user')
@@ -22,6 +24,7 @@ class AddToAlertDashboardController(Controller):
             action and action['res_model'] == 'board.board' and
             action['views'][0][1] == 'form' and action_id
         ):
+            # Maybe should check the content instead of model board.board ?
             view_id = action['views'][0][0]
             board = request.env['board.board'].fields_view_get(view_id, 'form')
             if board and 'arch' in board:
@@ -29,18 +32,18 @@ class AddToAlertDashboardController(Controller):
                 column = xml.find('./board/column')
                 if column is not None:
                     new_action = ElementTree.Element('action', {
-                        'context': str(context_to_save),
-                        'domain': str(domain),
                         'name': str(action_id),
                         'string': name,
                         'view_mode': view_mode,
+                        'context': str(context_to_save),
+                        'domain': str(domain)
                     })
                     column.insert(0, new_action)
-                    arch = ElementTree.tostring(xml, 'utf-8')
+                    arch = ElementTree.tostring(xml, encoding='unicode')
                     request.env['ir.ui.view.custom'].create({
-                        'arch': arch,
-                        'ref_id': view_id,
                         'user_id': request.session.uid,
+                        'ref_id': view_id,
+                        'arch': arch
                     })
                     return True
 
--- static/src/js/alert_board.js
+++ static/src/js/alert_board.js
@@ -1,8 +1,7 @@
-// TODO migration: Implementation in odoo 11 js changed. FIX IT
 /* Alert board. */
 
 /* Taken from the default "My board" dashboard.
- * Ref: odoo/addons/board/static/js/dashboard.js.
+ * Ref: odoo/addons/board/static/src/js/favorite_menu.js.
  *
  * Adapted to add an "Add to my alert board" menu command.
  */
@@ -11,18 +10,19 @@ odoo.define('board_alerts.alert_board', function(require) {
     'use strict';
 
     var ActionManager = require('web.ActionManager');
+    var Context = require('web.Context');
     var core = require('web.core');
-    var data = require('web.data');
+    var Domain = require('web.Domain');
     var FavoriteMenu = require('web.FavoriteMenu');
-    var Model = require('web.DataModel');
     var pyeval = require('web.pyeval');
     var ViewManager = require('web.ViewManager');
-    var data_manager = require('web.data_manager');
 
     var _t = core._t;
     var QWeb = core.qweb;
 
     FavoriteMenu.include({
+
+    // Same as the base "start" function.
     start: function() {
         var self = this;
         if (this.action_id === undefined) {
@@ -43,68 +43,77 @@ odoo.define('board_alerts.alert_board', function(require) {
             this.$add_alert_dashboard_input.val(title);
             this.$add_alert_dashboard_link.click(function(e) {
                 e.preventDefault();
-                self.toggle_alert_dashboard_menu();
+                self._toggleAlertDashboardMenu();
             });
-            this.$add_alert_dashboard_btn.click(this.proxy('add_alert_dashboard'));
+            this.$add_alert_dashboard_btn.click(this.proxy('_addAlertDashboard'));
         }
         return this._super();
     },
-    toggle_alert_dashboard_menu: function(is_open) {
-        this.$add_alert_dashboard_link.toggleClass(
-            'o_closed_menu',
-            !(_.isUndefined(is_open)) ? !is_open : undefined
-        ).toggleClass('o_open_menu', is_open);
-        this.$add_to_alert_dashboard.toggle(is_open);
-        if (this.$add_alert_dashboard_link.hasClass('o_open_menu')) {
-            this.$add_alert_dashboard_input.focus();
-        }
-    },
-    close_menus: function() {
-        if (this.add_to_alert_dashboard_available) {
-            this.toggle_alert_dashboard_menu(false);
-        }
-        this._super();
-    },
-    add_alert_dashboard: function() {
-        var self = this;
 
-        var search_data = this.searchview.build_search_data(),
-            context = new data.CompoundContext(this.searchview.dataset.get_context() || []),
-            domain = new data.CompoundDomain(this.searchview.dataset.get_domain() || []);
+    // Same as the base "_addDashboard" function.
+    _addAlertDashboard: function() {
+        var self = this;
+        var search_data = this.searchview.build_search_data();
+        var context = new Context(this.searchview.dataset.get_context() || []);
+        var domain = this.searchview.dataset.get_domain() || [];
+
         _.each(search_data.contexts, context.add, context);
-        _.each(search_data.domains, domain.add, domain);
+        _.each(search_data.domains, function(d) {
+            domain.push.apply(domain, Domain.prototype.stringToArray(d));
+        });
 
         context.add({
             group_by: pyeval.eval('groupbys', search_data.groupbys || [])
         });
-        context.add(this.view_manager.active_view.controller.get_context());
+        context.add(this.view_manager.active_view.controller.getContext());
         var c = pyeval.eval('context', context);
         for ( var k in c) {
             if (c.hasOwnProperty(k) && /^search_default_/.test(k)) {
                 delete c[k];
             }
         }
-        this.toggle_alert_dashboard_menu(false);
+        this._toggleAlertDashboardMenu(false);
         c.dashboard_merge_domains_contexts = false;
-        var d = pyeval.eval('domain', domain),
-            board = new Model('board.board'),
-            name = self.$add_alert_dashboard_input.val();
 
-        return self.rpc('/board/add_to_alert_dashboard', {
+        var name = self.$add_alert_dashboard_input.val();
+
+        return self._rpc({
+        route: '/board/add_to_alert_dashboard',
+        params: {
         action_id: self.action_id || false,
         context_to_save: c,
-        domain: d,
+        domain: domain,
         view_mode: self.view_manager.active_view.type,
         name: name,
+        },
         }).then(function(r) {
             if (r) {
                 self.do_notify(_.str.sprintf(_t("'%s' added to alert dashboard"), name), '');
-                data_manager.invalidate();
             } else {
                 self.do_warn(_t("Could not add filter to alert dashboard"));
             }
         });
     },
+
+    // Same as the base "_closeMenus" function.
+    _closeMenus: function() {
+        if (this.add_to_alert_dashboard_available) {
+            this._toggleAlertDashboardMenu(false);
+        }
+        this._super();
+    },
+
+    // Same as the base "_toggleDashboardMenu" function.
+    _toggleAlertDashboardMenu: function(isOpen) {
+        this.$add_alert_dashboard_link // nowrap
+        .toggleClass('o_closed_menu', !(_.isUndefined(isOpen)) ? !isOpen : undefined) // nowrap
+        .toggleClass('o_open_menu', isOpen);
+        this.$add_to_alert_dashboard.toggle(isOpen);
+        if (this.$add_alert_dashboard_link.hasClass('o_open_menu')) {
+            this.$add_alert_dashboard_input.focus();
+        }
+    },
+
     });
 
 });
