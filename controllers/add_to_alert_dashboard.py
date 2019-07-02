16424141108a45264c399869d10a457ba108eea8
Etienne Ferriere <etienne.ferriere@xcg-consulting.fr>
2019-07-02 17:35:14 +0200

    "Executed ``black``."
    --HG--
    branch : 10.0

--- controllers/add_to_alert_dashboard.py
+++ controllers/add_to_alert_dashboard.py
@@ -10,38 +10,45 @@ class AddToAlertDashboardController(Controller):
     Similar to the one for the default dashboard in the "board" module.
     """
 
-    @route(route='/board/add_to_alert_dashboard', type='json', auth='user')
+    @route(route="/board/add_to_alert_dashboard", type="json", auth="user")
     def add_to_dashboard(
-        self, action_id, context_to_save, domain, view_mode, name='',
+        self, action_id, context_to_save, domain, view_mode, name=""
     ):
         """Called when adding to the alert dashboard from the Odoo web client.
         """
 
-        action = request.env.ref('board_alerts.action_alert_board')
+        action = request.env.ref("board_alerts.action_alert_board")
         if (
-            action and action['res_model'] == 'board.board' and
-            action['views'][0][1] == 'form' and action_id
+            action
+            and action["res_model"] == "board.board"
+            and action["views"][0][1] == "form"
+            and action_id
         ):
-            view_id = action['views'][0][0]
-            board = request.env['board.board'].fields_view_get(view_id, 'form')
-            if board and 'arch' in board:
-                xml = ElementTree.fromstring(board['arch'])
-                column = xml.find('./board/column')
+            view_id = action["views"][0][0]
+            board = request.env["board.board"].fields_view_get(view_id, "form")
+            if board and "arch" in board:
+                xml = ElementTree.fromstring(board["arch"])
+                column = xml.find("./board/column")
                 if column is not None:
-                    new_action = ElementTree.Element('action', {
-                        'context': str(context_to_save),
-                        'domain': str(domain),
-                        'name': str(action_id),
-                        'string': name,
-                        'view_mode': view_mode,
-                    })
+                    new_action = ElementTree.Element(
+                        "action",
+                        {
+                            "context": str(context_to_save),
+                            "domain": str(domain),
+                            "name": str(action_id),
+                            "string": name,
+                            "view_mode": view_mode,
+                        },
+                    )
                     column.insert(0, new_action)
-                    arch = ElementTree.tostring(xml, 'utf-8')
-                    request.env['ir.ui.view.custom'].create({
-                        'arch': arch,
-                        'ref_id': view_id,
-                        'user_id': request.session.uid,
-                    })
+                    arch = ElementTree.tostring(xml, "utf-8")
+                    request.env["ir.ui.view.custom"].create(
+                        {
+                            "arch": arch,
+                            "ref_id": view_id,
+                            "user_id": request.session.uid,
+                        }
+                    )
                     return True
 
         return False
--- wizard/board_alerts_dlg.py
+++ wizard/board_alerts_dlg.py
@@ -6,26 +6,26 @@ class BoardAlertsDlg(models.TransientModel):
     kind of confirmation.
     """
 
-    _name = 'board_alerts_dlg'
-    _description = 'Board alert sender dialog box'
+    _name = "board_alerts_dlg"
+    _description = "Board alert sender dialog box"
 
     @api.multi
     def send_board_alerts(self):
         """Send board alerts then show emails.
         """
 
-        self.env['res.users'].send_board_alerts()
+        self.env["res.users"].send_board_alerts()
 
         # Find the action launched by the "Emails" menu command.
-        emails_action = self.env.ref('mail.menu_mail_mail').action
+        emails_action = self.env.ref("mail.menu_mail_mail").action
 
         return {
-            'context': emails_action.context,
-            'name': emails_action.name,
-            'res_model': emails_action.res_model,
-            'search_view_id': emails_action.search_view_id.id,
-            'target': emails_action.target,
-            'type': emails_action.type,
-            'view_mode': emails_action.view_mode,
-            'view_type': emails_action.view_type,
+            "context": emails_action.context,
+            "name": emails_action.name,
+            "res_model": emails_action.res_model,
+            "search_view_id": emails_action.search_view_id.id,
+            "target": emails_action.target,
+            "type": emails_action.type,
+            "view_mode": emails_action.view_mode,
+            "view_type": emails_action.view_type,
         }
